567e5b58f849b8ab1aff3630dc00808d8d3b054a
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-07-22 18:53:47 +0200

    create html cards-section1 and add landing in gulp as entry point

--- gulpfile.js
+++ gulpfile.js
@@ -172,7 +172,8 @@ gulp.task('icons-dist', function(done) {
 gulp.task('default', gulp.series(['clean','html', 'styles','scripts', 'images', 'icons'], function(done) {
   browserSync.init({
     server : {
-      baseDir: './public/'
+      baseDir: './public/',
+      index: "landing.html"
     }
   });
   gulp.watch(config.watch.html, gulp.series(['html', 'bs-reload']));
