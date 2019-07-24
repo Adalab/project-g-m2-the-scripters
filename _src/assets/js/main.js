9e6aaef78d01fee80d45e0b7805a446a7ac4fa25
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-07-24 16:59:18 +0200

    make button collapsable function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -8,19 +8,29 @@ const clickDesign = document.querySelector('.design-collapsable');
 const clickFill = document.querySelector('.fill-collapsable');
 const clickSubmit = document.querySelector('.submit-collapsable');
 
+const clickShare = document.querySelector('.collapsebtn');
+
 const collapseDesign = document.querySelector('.design-container');
 const collapseFill = document.querySelector('.fill-container');
 const collapseSubmit = document.querySelector('.submit-container');
+
+const collapseShare = document.querySelector('.share-container');
+
 
 //FUNCIÓN
 function openCollapsable (event) {
   collapseDesign.classList.toggle ('hidden');
 }
 function openCollapsable2 (event) {
-collapseFill.classList.toggle ('hidden');
+  collapseFill.classList.toggle ('hidden');
 }
 function openCollapsable3 (event) {
   collapseSubmit.classList.toggle ('hidden');
+}
+
+function openCollapsable4 (event) {
+  event.preventDefault ();
+  collapseShare.classList.toggle ('hidden');
 }
 
 //Listeners
@@ -28,3 +38,4 @@ clickDesign.addEventListener('click', openCollapsable);
 clickFill.addEventListener('click', openCollapsable2);
 clickSubmit.addEventListener('click', openCollapsable3);
 
+clickShare.addEventListener('click', openCollapsable4);
