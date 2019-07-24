682156c1044c757f31ff36278f483a5b2ce39d99
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-07-24 15:54:29 +0200

    make collapsables with javascript

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+
+//Crear constante con las etiquetas de HTML
+/* const clickDesign = document.querySelector('.design-collapsable');
+const clickFill = document.querySelector('.fill-collapsable');
+const clickShare = document.querySelector('.share-collapsable');
+
+const collapsing = document.querySelector('.collapsable');
+ */
+
+//FUNCIÓN
+/* function openCollapsable (event) {
+    const targingCollapsable = event.currentTarget.innerHTML;
+  if (collapsing.classList.contains ('hidden') === true) {
+      collapsing.classList.remove('hidden');
+  } else {
+      collapsing.classList.add('hidden');
+  }
+} */
+
+
+
+//Listeners
+/*clickDesign.addEventListener('click', openCollapsable);
+ clickFill.addEventListener('click', openCollapsable);clickShare.addEventListener('click', openCollapsable);
+ */
