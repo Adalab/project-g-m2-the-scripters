a186d58dbfa06bac9c49e16ea1599bdf727f4073
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-07-24 16:15:16 +0200

    make first function work

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,26 +4,27 @@ console.log('>> Ready :)');
 
 
 //Crear constante con las etiquetas de HTML
-/* const clickDesign = document.querySelector('.design-collapsable');
+const clickDesign = document.querySelector('.design-collapsable');
 const clickFill = document.querySelector('.fill-collapsable');
 const clickShare = document.querySelector('.share-collapsable');
 
 const collapsing = document.querySelector('.collapsable');
- */
+
 
 //FUNCIÓN
-/* function openCollapsable (event) {
+function openCollapsable (event) {
     const targingCollapsable = event.currentTarget.innerHTML;
   if (collapsing.classList.contains ('hidden') === true) {
       collapsing.classList.remove('hidden');
   } else {
       collapsing.classList.add('hidden');
   }
-} */
+}
 
 
 
 //Listeners
-/*clickDesign.addEventListener('click', openCollapsable);
- clickFill.addEventListener('click', openCollapsable);clickShare.addEventListener('click', openCollapsable);
- */
+clickDesign.addEventListener('click', openCollapsable);
+clickFill.addEventListener('click', openCollapsable);
+clickShare.addEventListener('click', openCollapsable);
+
