a7b87bc36d0bdc0486b793aee5d1b883564d678b
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-07-25 14:06:51 +0200

    create color functions

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,63 +2,36 @@
 
 console.log('>> Ready :)');
 
-//Crear constante con las etiquetas de HTML
-const clickDesign = document.querySelector('.design-collapsable');
-const clickFill = document.querySelector('.fill-collapsable');
-const clickSubmit = document.querySelector('.submit-collapsable');
+const clickTheme1 = document.querySelector('.theme__radio1');
+const clickTheme2 = document.querySelector('.theme__radio2');
+const clickTheme3 = document.querySelector('.theme__radio3');
 
-const clickShare = document.querySelector('.collapsebtn');
+const card = document.querySelector('.section1__card');
 
-const collapseDesign = document.querySelector('.design-container');
-const collapseFill = document.querySelector('.fill-container');
-const collapseSubmit = document.querySelector('.submit-container');
+function changeColor1 () {
+    card.classList.remove ('theme-colors1');
+    card.classList.remove ('theme-colors2');
+    card.classList.remove ('theme-colors3');
 
-const collapseShare = document.querySelector('.share-container');
-
-
-//FUNCIÓN
-const openCollapsable = () => {
-  collapseDesign.classList.toggle ('hidden');
-  collapseFill.classList.add ('hidden');
-  collapseSubmit.classList.add('hidden')
-}
-const openCollapsable2 = () => {
-  collapseDesign.classList.add ('hidden');
-  collapseFill.classList.toggle ('hidden');
-  collapseSubmit.classList.add ('hidden')
-}
-const openCollapsable3 = () => {
-  collapseDesign.classList.add ('hidden');
-  collapseFill.classList.add ('hidden');
-  collapseSubmit.classList.toggle ('hidden')
+    card.classList.add ('theme-colors1');
 }
 
-const openCollapsable4 = () => {
-  event.preventDefault ();
-  collapseShare.classList.toggle ('hidden');
-  clickShare.classList.add('button-Create-after');
+function changeColor2 () {
+  card.classList.remove ('theme-colors1');
+  card.classList.remove ('theme-colors2');
+  card.classList.remove ('theme-colors3');
+
+  card.classList.add ('theme-colors2');
 }
 
-/*
-function openCollapsable (event) {
-  collapseDesign.classList.toggle ('hidden');
-}
-function openCollapsable2 (event) {
-  collapseFill.classList.toggle ('hidden');
-}
-function openCollapsable3 (event) {
-  collapseSubmit.classList.toggle ('hidden');
+function changeColor3 () {
+  card.classList.remove ('theme-colors1');
+  card.classList.remove ('theme-colors2');
+  card.classList.remove ('theme-colors3');
+
+  card.classList.add ('theme-colors3');
 }
 
-function openCollapsable4 (event) {
-  event.preventDefault ();
-  collapseShare.classList.toggle ('hidden');
-  clickShare.classList.add('button-Create-after');
-}*/
-
-//Listeners
-clickDesign.addEventListener('click', openCollapsable);
-clickFill.addEventListener('click', openCollapsable2);
-clickSubmit.addEventListener('click', openCollapsable3);
-
-clickShare.addEventListener('click', openCollapsable4);
+clickTheme1.addEventListener('click', changeColor1);
+clickTheme2.addEventListener('click', changeColor2);
+clickTheme3.addEventListener('click', changeColor3);
