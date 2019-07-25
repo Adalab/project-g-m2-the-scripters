42f800ce9c3f9ec261b5a4df316bdd7b2bd86349
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-07-25 16:38:57 +0200

    Add only one function to themes

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -67,34 +67,37 @@ const clickTheme1 = document.querySelector('.theme__radio1');
 const clickTheme2 = document.querySelector('.theme__radio2');
 const clickTheme3 = document.querySelector('.theme__radio3');
 
+
 //FUNCIONES COLORES
 
 const card = document.querySelector('.section1__card');
 
-function changeColor1 () {
+function changeColor (event){
+  const tema = event.currentTarget.value;
+
+  if(event.currentTarget.value === 'theme1'){
     card.classList.remove ('theme-colors1');
     card.classList.remove ('theme-colors2');
     card.classList.remove ('theme-colors3');
 
     card.classList.add ('theme-colors1');
+  }
+  else if (event.currentTarget.value === 'theme2'){
+    card.classList.remove ('theme-colors1');
+    card.classList.remove ('theme-colors2');
+    card.classList.remove ('theme-colors3');
+
+    card.classList.add ('theme-colors2');
+  }
+  else{
+    card.classList.remove ('theme-colors1');
+    card.classList.remove ('theme-colors2');
+    card.classList.remove ('theme-colors3');
+
+    card.classList.add ('theme-colors3');
+  }
 }
 
-function changeColor2 () {
-  card.classList.remove ('theme-colors1');
-  card.classList.remove ('theme-colors2');
-  card.classList.remove ('theme-colors3');
-
-  card.classList.add ('theme-colors2');
-}
-
-function changeColor3 () {
-  card.classList.remove ('theme-colors1');
-  card.classList.remove ('theme-colors2');
-  card.classList.remove ('theme-colors3');
-
-  card.classList.add ('theme-colors3');
-}
-
-clickTheme1.addEventListener('click', changeColor1);
-clickTheme2.addEventListener('click', changeColor2);
-clickTheme3.addEventListener('click', changeColor3);
+clickTheme1.addEventListener('click', changeColor);
+clickTheme2.addEventListener('click', changeColor);
+clickTheme3.addEventListener('click', changeColor);
