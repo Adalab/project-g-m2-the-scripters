7c96254431995e849bde4c6a236d1f7bf340698e
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-08-01 11:52:35 +0200

    create fetch function and form-object

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -258,3 +258,51 @@ function fakeFileClick() {
  */
 uploadBtn.addEventListener('click', fakeFileClick);
 fileField.addEventListener('change', getImage);
+
+//PETICIONES AL SERVIDOR
+
+/*const arrayForm = {
+    "palette": input.value,
+    "name": input.value,
+    "job": input.value,
+    "phone": input.value,
+    "email": input.value,
+    "linkedin": input.value,
+    "github": input.value,
+    "photo": `${fr.result}`,
+  };
+  */
+
+const arrayForms = {
+  "palette": 1,
+  "name": "María García",
+  "job": "Front-end developer",
+  "phone": "+34 666666666",
+  "email": "mariagar@example.com",
+  "linkedin": "mariagar",
+  "github": "mariagar",
+  "photo": "data:image/png;base64,2342ba..."
+};
+
+function sendRequest(json){
+  fetch("https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/", {
+    method: 'POST',
+    body: JSON.stringify(json),
+    headers: {
+      'content-type': 'application/json'
+    },
+  })
+    .then(function(resp) { return resp.json(); })
+    .then(data => {console.log(data)});
+}
+
+console.log(sendRequest(arrayForms));
+/*
+var submitButton = document.querySelector('#submit');
+var responseURL = document.querySelector('.js__response');
+var form = document.querySelector('#form');
+var fr = new FileReader();
+
+submitButton.addEventListener('click', loadPhoto);
+*/
+
