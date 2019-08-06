7910eaf45f0cc1168a4d35837d41f508527df710
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-08-06 11:09:26 +0200

    Separate get-avata library

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -19,7 +19,6 @@ const clickTheme2 = document.querySelector('.theme__radio2');
 const clickTheme3 = document.querySelector('.theme__radio3');
 const imageCardContainer = document.querySelector ('.section1__image');
 const previewImage = document.querySelector ('.form-photo-preview');
-const imgCard = document.querySelector ('#img');
 let resultColorValue;
 let savedData;
 
@@ -277,60 +276,6 @@ function resetCard() {
 
 resetButton.addEventListener('click', resetCard);
 
-//IMAGEN PREVISUALIZACIÓN
-
-const fr = new FileReader();
-const uploadBtn = document.querySelector('.js__profile-trigger');
-const fileField = document.querySelector('.js__profile-upload-btn');
-const profileImage = document.querySelector('.js__profile-image');
-const profilePreview = document.querySelector('.js__profile-preview');
-
-
-/**
- * Recoge el archivo añadido al campo de tipo "file"
- * y lo carga en nuestro objeto FileReader para que
- * lo convierta a algo con lo que podamos trabajar.
- * Añade un listener al FR para que ejecute una función
- * al tener los datos listos
- * @param {evento} e
- */
-function getImage(e){
-  var myFile = e.currentTarget.files[0];
-  fr.addEventListener('load', writeImage);
-  fr.readAsDataURL(myFile);
-}
-
-
-/**
- * Una vez tenemos los datos listos en el FR podemos
- * trabajar con ellos ;)
- */
-function writeImage() {
-  /* En la propiedad `result` de nuestro FR se almacena
-   * el resultado
-   */
-  profileImage.style.backgroundImage = `url(${fr.result})`;
-  profilePreview.style.backgroundImage = `url(${fr.result})`;
-  imgCard.src=`${fr.result}`;
-  localStorage.setItem('image', fr.result);
-}
-
-
-/**
- * Genera un click automático en nuesto campo de tipo "file"
- * que está oculto
- */
-function fakeFileClick() {
-  fileField.click();
-}
-
-/**
- * Añadimos los listeners necesarios:
- * - al botón visible para generar el click automático
- * - al campo oculto para cuando cambie su value
- */
-uploadBtn.addEventListener('click', fakeFileClick);
-fileField.addEventListener('change', getImage);
 
 //PETICIONES AL SERVIDOR
 
